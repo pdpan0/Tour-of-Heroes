22826d2e0fa351e98630d4e1afe6ff78946b4ae0
Lucas Martins <lucas.martins@VALEMOBI.CORP@VMNOTE-258.valemobi.corp>
2021-08-14 22:06:32 -0300

    FEAT (*) Estruturação do app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
+/*
+  Arquivo responsável pelos módulos do app.
+*/
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Habilita [(ngModel)] para formulários.
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
+  // Todos componentes devem ser declarados.
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
